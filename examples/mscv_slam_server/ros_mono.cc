9a856e8e248333f69b8ebe1e19d458568bde4554
leonana69 <chengj0803@gmail.com>
2023-02-08 16:58:07 -0500

    update

---
+++ examples/mscv_slam_server/ros_mono.cc
@@ -0,0 +1,154 @@
+/**
+* This file is part of ORB-SLAM3
+*
+* Copyright (C) 2017-2020 Carlos Campos, Richard Elvira, Juan J. Gómez Rodríguez, José M.M. Montiel and Juan D. Tardós, University of Zaragoza.
+* Copyright (C) 2014-2016 Raúl Mur-Artal, José M.M. Montiel and Juan D. Tardós, University of Zaragoza.
+*
+* ORB-SLAM3 is free software: you can redistribute it and/or modify it under the terms of the GNU General Public
+* License as published by the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* ORB-SLAM3 is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even
+* the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License along with ORB-SLAM3.
+* If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#include<iostream>
+#include<algorithm>
+#include<fstream>
+#include<chrono>
+
+#include<ros/ros.h>
+#include <cv_bridge/cv_bridge.h>
+
+#include<opencv2/core/core.hpp>
+
+#include"../../../include/System.h"
+
+// leo: for pubbing
+#include"Converter.h"
+#include <geometry_msgs/PoseStamped.h>
+#include <geometry_msgs/Point.h>
+#include <geometry_msgs/Quaternion.h>
+#include <tf/transform_broadcaster.h>
+// leo: end
+
+using namespace std;
+
+class ImageGrabber
+{
+public:
+    ImageGrabber(ORB_SLAM3::System* pSLAM):mpSLAM(pSLAM){}
+
+    void GrabImage(const sensor_msgs::ImageConstPtr& msg);
+
+    ORB_SLAM3::System* mpSLAM;
+
+    // leo: add for pub
+    void SetPub(ros::Publisher* pub) {
+        orb_pub = pub;
+    }
+    cv::Mat m1, m2;
+    bool do_rectify, pub_tf, pub_pose;
+    ros::Publisher* orb_pub;
+    // leo: end
+};
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "Mono");
+    ros::start();
+
+    if(argc != 3)
+    {
+        cerr << endl << "Usage: rosrun ORB_SLAM3 Mono path_to_vocabulary path_to_settings" << endl;        
+        ros::shutdown();
+        return 1;
+    }
+
+    // Create SLAM system. It initializes all system threads and gets ready to process frames.
+    ORB_SLAM3::System SLAM(argv[1],argv[2],ORB_SLAM3::System::MONOCULAR,true);
+
+    ImageGrabber igb(&SLAM);
+
+    ros::NodeHandle nodeHandler;
+    ros::Subscriber sub = nodeHandler.subscribe("/camera/image_raw", 1, &ImageGrabber::GrabImage,&igb);
+
+    // leo: add for pub
+    ros::Publisher pose_pub = nodeHandler.advertise<geometry_msgs::PoseStamped>("orb_pose", 100);
+    igb.SetPub(&pose_pub);
+    // leo: end
+
+    ros::spin();
+
+    // Stop all threads
+    SLAM.Shutdown();
+
+    // Save camera trajectory
+    SLAM.SaveKeyFrameTrajectoryTUM("KeyFrameTrajectory.txt");
+
+    ros::shutdown();
+
+    return 0;
+}
+
+void ImageGrabber::GrabImage(const sensor_msgs::ImageConstPtr& msg)
+{
+    // Copy the ros image message to cv::Mat.
+    cv_bridge::CvImageConstPtr cv_ptr;
+    try
+    {
+        cv_ptr = cv_bridge::toCvShare(msg);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+        ROS_ERROR("cv_bridge exception: %s", e.what());
+        return;
+    }
+
+    cv::Mat T_, R_, t_ ;
+
+    T_ = mpSLAM->TrackMonocular(cv_ptr->image,cv_ptr->header.stamp.toSec());
+
+    if (pub_tf || pub_pose)
+    {    
+        if (!(T_.empty())) {
+
+            cv::Size s = T_.size();
+            if ((s.height >= 3) && (s.width >= 3)) {
+                R_ = T_.rowRange(0,3).colRange(0,3).t();
+                t_ = -R_*T_.rowRange(0,3).col(3);
+                vector<float> q = ORB_SLAM3::Converter::toQuaternion(R_);
+                float scale_factor=1.0;
+                tf::Transform transform;
+                transform.setOrigin(tf::Vector3(t_.at<float>(0, 0)*scale_factor, t_.at<float>(0, 1)*scale_factor, t_.at<float>(0, 2)*scale_factor));
+                tf::Quaternion tf_quaternion(q[0], q[1], q[2], q[3]);
+                transform.setRotation(tf_quaternion);
+                /*
+                if (pub_tf)
+                {
+                static tf::TransformBroadcaster br_;
+                br_.sendTransform(tf::StampedTransform(transform, ros::Time(tIm), "world", "ORB_SLAM3_MONO_INERTIAL"));
+                }
+                */
+
+                if (pub_pose)
+                {
+                    geometry_msgs::PoseStamped pose;
+                    //pose.header.stamp = img0Buf.front()->header.stamp;
+                    pose.header.frame_id ="ORB_SLAM3_MONO_INERTIAL";
+                    tf::poseTFToMsg(transform, pose.pose);
+                    orb_pub->publish(pose);
+                }
+
+            }
+        }
+    }
+
+}
+
+
--- src/multisemantic_ros_server/slam_task.py
+++ src/multisemantic_ros_server/slam_task.py
@@ -1,12 +1,20 @@
 import rospy
 from sensor_msgs.msg import Image
+from geometry_msgs.msg import PoseStamped
 
 class SLAMTask():
 	def __init__(self):
 		self.pub = rospy.Publisher('/camera/image_raw', Image, queue_size=1)
+		self.sub = rospy.Subscriber('/orb_pose', geometry_msgs::PoseStamped, SLAMTask::slam_callback, self)
+		self.pose = []
 
 	def request(self, image_msg):
 		if not rospy.is_shutdown():
 			self.pub.publish(image_msg)
 		else:
 			print('[SL] ros is down')
+	
+	def slam_callback(data, slam_task):
+		print('get one pose')
+		slam_task.pose.append(data)
+		print('append one pose')
