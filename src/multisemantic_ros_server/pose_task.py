5d09aa8b947ffc2a0eade96ccad208356ed11358
leonana69 <chengj0803@gmail.com>
2023-02-09 09:29:18 -0500

    update

--- src/multisemantic_ros_server/pose_task.py
+++ src/multisemantic_ros_server/pose_task.py
@@ -4,8 +4,12 @@ from multisemantic_ros_server.srv import MSCVPose
 
 class PoseTask():
     def __init__(self):
-        rospy.wait_for_service('mscv_pose_service')
-
+        try:
+            mscv_pose = rospy.ServiceProxy('mscv_pose_service', MSCVPose)
+            print("[PT] mscv_pose_service is up")
+        except rospy.ServiceException as e:
+            print("[PT] mscv_pose_service not present")
+            
     def request(self, image_msg):
         try:
             mscv_pose = rospy.ServiceProxy('mscv_pose_service', MSCVPose)
